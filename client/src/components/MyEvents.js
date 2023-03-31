c994f41c5f498832527074fc3cc56bf96434b926
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-03-31 13:28:31 -0400

    added hover on myEvents pages

--- client/src/components/MyEvents.js
+++ client/src/components/MyEvents.js
@@ -3,18 +3,34 @@ import { useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { Container, Button, Card, Row, Col, Circle } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlus,
+  faPenToSquare,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 
 import placeholder from "../assets/placeholder.png";
 import { GET_ONE_USER } from "../utils/queries";
 import Auth from "../utils/auth";
 
 const MyEvents = () => {
+  // adding hover feature
+  const [hover, setHover] = useState(null);
+
+  const onHover = (eventId) => {
+    setHover(eventId);
+  };
+
+  const onLeave = () => {
+    setHover(null);
+  };
+
   //getting the loggedin user's ID
   const userToken = Auth.getProfile();
   const userId = userToken.data._id;
   console.log(userId);
 
+  //retrieving logged in users data
   const { loading, error, data } = useQuery(GET_ONE_USER, {
     variables: { userId: userId },
   });
@@ -22,55 +38,103 @@ const MyEvents = () => {
   if (error) console.log(error.message);
   console.log(data);
 
-  const { user } = data || [];
+  const { user } = data || {};
 
   if (user) {
-    const events = user.events || "No Events";
+    const events = user.events;
     console.log(events);
+
+    const eventHoverStyle = {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: "40%",
+      zIndex: "1",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    };
 
     return (
       <>
         {loading ? (
+          // if GET_ONE_USER is loading send this:
           <div>
             <h3>still loading</h3>
           </div>
         ) : (
+          // once GET_ONE_USER data is loaded, send this:
           <Container className="center">
             <div className="mx-auto p-5">
               <h3>Welcome {user.username}!</h3>
               <div>
-                <h4 className="mt-5">Here are your Events:</h4>
-                <p>To edit or delete events, hover over the image</p>
-                {/* rendering user's events */}
-                <Container>
-                  <Row xs={1} md={2} lg={3} className="g-4">
-                    {events.map((event) => (
-                      <Col key={event._id}>
-                        <Card style={{ width: "18rem" }}>
-                          <Card.Img
-                            variant="top"
-                            src={placeholder}
-                            className="p-2"
-                          />
-                          <Card.Body>
-                            <Card.Title>{event.title}</Card.Title>
-                            <Card.Text>{event.description}</Card.Text>
-                            {/* Remember to link proper event details page */}
-                            <Button
-                              as={Link}
-                              to="/eventDetail"
-                              variant="primary"
+                {/* conditional rendering if the user has events */}
+                {events ? (
+                  <>
+                    <h4 className="mt-5">Here are your Events:</h4>
+                    <p>To edit or delete events, hover over the image</p>
+                    {/* rendering user's events with map method*/}
+                    <Container>
+                      <Row xs={1} md={2} lg={3} className="g-4">
+                        {events.map((event) => (
+                          <Col key={event.id}>
+                            <Card
+                              onMouseEnter={() => onHover(event.id)}
+                              onMouseLeave={() => onLeave(null)}
+                              style={{ width: "18rem" }}
                             >
-                              more details
-                            </Button>
-                          </Card.Body>
-                        </Card>
-                      </Col>
-                    ))}
-                  </Row>
-                </Container>
+                              <Card.Img
+                                variant="top"
+                                src={placeholder}
+                                className="p-2"
+                              />
+                              {/* update and delete icon appear on hover */}
+                              {hover === event.id && (
+                                <div style={eventHoverStyle}>
+                                  <Button variant="link" as={Link} to="/update">
+                                    <FontAwesomeIcon
+                                      icon={faPenToSquare}
+                                      size="2x"
+                                    />
+                                  </Button>
+                                  <Button variant="link" as={Link} to="/delete">
+                                    <FontAwesomeIcon
+                                      icon={faTrashCan}
+                                      size="2x"
+                                    />
+                                  </Button>
+                                </div>
+                              )}
+                              <Card.Body>
+                                <Card.Title>{event.title}</Card.Title>
+                                <Card.Text>{event.description}</Card.Text>
+                                {/* Remember to link proper event details page */}
+                                <Button
+                                  as={Link}
+                                  to="/eventDetail"
+                                  variant="primary"
+                                >
+                                  more details
+                                </Button>
+                              </Card.Body>
+                            </Card>
+                          </Col>
+                        ))}
+                      </Row>
+                    </Container>
+                  </>
+                ) : (
+                  // if the user has no events:
+                  <>
+                    <h4 className="mt-5">You haven't made any events yet!</h4>
+                    <p>
+                      To create an event, click the + icon on the bottom right
+                    </p>
+                  </>
+                )}
               </div>
-              {/* links to create event on click */}
+              {/* link to create event on click */}
               <Button
                 as={Link}
                 to="/createEvent"
@@ -83,13 +147,38 @@ const MyEvents = () => {
                   width: "50px",
                   height: "50px",
                 }}
+                key="addEventBtn"
+                onMouseEnter={() => onHover("addEventBtn")}
+                onMouseLeave={() => onLeave(null)}
               >
                 <FontAwesomeIcon icon={faPlus} size="2x" />
+                {/* hover not working yet but it's not a main functionality */}
+                {hover === "#addEventBtn" && (
+                  <h2
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: "40%",
+                      zIndex: "2",
+                    }}
+                  >
+                    {console.log("here")}
+                    hello
+                  </h2>
+                )}
               </Button>
             </div>
           </Container>
         )}
       </>
+    );
+  } else {
+    return (
+      <div>
+        <h3>Sorry, your events could not be found! </h3>
+      </div>
     );
   }
 };
@@ -98,51 +187,3 @@ export default MyEvents;
 //edit icon: https://fontawesome.com/icons/pen-to-square?f=classic&s=regular
 //trash icon: https://fontawesome.com/icons/trash-can?f=classic&s=solid
 //plus icon: https://fontawesome.com/icons/plus?f=classic&s=solid
-
-// DASHBOARD CHESS BLOG
-// DASHBOARD CHESS BLOG
-// DASHBOARD CHESS BLOG
-// DASHBOARD CHESS BLOG
-
-// import React from "react";
-
-// const Dashboard = ({ posts, format_date }) => {
-//   return (
-//     <main className="container mt-5">
-//       <div className="row">
-//         <div className="col-12">
-//           <h1 className="mb-4">Dashboard</h1>
-//           <a href="/newpost" className="btn btn-primary">
-//             Create a New Blog Post
-//           </a>
-//         </div>
-//       </div>
-//       <br />
-//       <h1 className="mb-4">Chess Blog Contributions:</h1>
-//       {posts.map((post) => (
-//         <div className="post-container post mb-3" key={post.id}>
-//           <h2>
-//             <a href={`/post/${post.id}`} className="text-decoration-none text-reset">
-//               {post.title}
-//             </a>
-//           </h2>
-//           <p>
-//             Created by: {post.user.username} | Date: {format_date(post.createdAt)}
-//           </p>
-//           <a href={`/editpost/${post.id}`} className="btn btn-warning">
-//             Edit Post
-//           </a>
-//           <button className="btn btn-danger delete-post" data-post-id={post.id}>
-//             Delete
-//           </button>
-//         </div>
-//       ))}
-//     </main>
-//   );
-// };
-
-// export default Dashboard;
-
-// DASHBOARD CHESS BLOG
-// DASHBOARD CHESS BLOG
-// DASHBOARD CHESS BLOG
