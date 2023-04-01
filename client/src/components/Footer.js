0266ca6bfa2633eec85c145b7a2643c029e615d9
devarsh2395 <devarsh2395@gmail.com>
2023-04-01 09:48:51 -0400

    added css styles

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -12,28 +12,28 @@ const Footer = () => {
         <section className="social-icons-wrapper d-flex flex-column flex-md-row justify-content-center my-3">
           <div className="d-flex flex-column align-items-center mx-md-2">
             <a href="https://github.com/jjocelynn" className="icon jjocelynn" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faGithub} size="lg" style={{ color: 'green' }} />
+              <FontAwesomeIcon icon={faGithub} size="lg" style={{ color: 'white' }} />
             </a>
-            <span>@jjocelynn</span>
+            <span>Jocelyn</span>
           </div>
           <div className="d-flex flex-column align-items-center mx-md-2">
             <a href="https://github.com/devarsh2395" className="icon devarsh2395" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faGithub} size="lg" style={{ color: 'purple' }} />
+              <FontAwesomeIcon icon={faGithub} size="lg" style={{ color: 'white' }} />
             </a>
-            <span>@devarsh2395</span>
+            <span>Devarsh</span>
           </div>
           <div className="d-flex flex-column align-items-center mx-md-2">
             <a href="https://github.com/ThomasCalle" className="icon ThomasCalle" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faGithub} size="lg" style={{ color: 'lightblue' }} />
+              <FontAwesomeIcon icon={faGithub} size="lg" style={{ color: 'white' }} />
             </a>
-            <span>@ThomasCalle</span>
+            <span>Thomas</span>
           </div>
         </section>
         <section className="footer" id="footer">
           <section className="form-footer">
-            <h5 className="text-center mb-2">Designed with ⏳ by UTOR 23-Project-3 Team: 8</h5>
+            {/* <h5 className="text-center mb-2">Designed with ⏳ by UTOR 23-Project-3 Team: 8</h5> */}
             <p className="text-center mb-0">
-              <h6>&copy; {new Date().getFullYear()} 2023 All rights reserved.</h6>
+              <h6>&copy; 2023 All rights reserved.</h6>
             </p>
           </section>
         </section>
