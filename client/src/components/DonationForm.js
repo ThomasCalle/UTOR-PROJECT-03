54541893b3394d356953a7e0f06cbbd9695ddffe
Thomas Calle <thos.calle@gmail.com>
2023-04-03 09:02:46 -0400

    background photos/file clean prep

--- client/src/components/DonationForm.js
+++ client/src/components/DonationForm.js
@@ -52,23 +52,3 @@ const DonationForm = (props) => {
 };
 
 export default injectStripe(DonationForm);
-
-
-
-// HOW TO REGISTER FOR FREEE
-// HOW TO REGISTER FOR FREEE
-// HOW TO REGISTER FOR FREEE
-
-// NO.1 Stripe:
-// Stripe is a payment processing platform, and it has a pay-as-you-go pricing model. You'll need a Stripe account to obtain your API keys and process payments.
-// Sign up for a Stripe account here: https://dashboard.stripe.com/register
-
-// Refer to Stripe's pricing details here: https://stripe.com/pricing
-
-// NO. 2 Google reCAPTCHA:
-// Google reCAPTCHA is a free service to protect your website from spam and abuse. It helps ensure that only humans can interact with your website by solving the CAPTCHA challenge.
-// Sign up for a Google reCAPTCHA account here: https://www.google.com/recaptcha/admin/create
-
-// Although Google reCAPTCHA is free to use, there are some usage limits. You can find more details here: https://developers.google.com/recaptcha/docs/faq
-
-// Once you've signed up for these services, you can obtain the necessary API keys and site keys to integrate them into your React application as described in the previous response.
